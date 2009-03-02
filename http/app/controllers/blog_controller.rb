1cb40d6a57bb458d8ccde233706198cfe1d4bb6a
Afa <afa.alone@gmail.com>
2009-03-02 23:06:23 +0000

    add Blog controller + User controller

---
+++ http/app/controllers/blog_controller.rb
@@ -0,0 +1,20 @@
+class BlogController < ApplicationController
+  def index
+  end
+
+  def post
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def show
+  end
+
+  def dated
+  end
+
+end
---
+++ http/app/controllers/user_controller.rb
@@ -0,0 +1,17 @@
+class UserController < ApplicationController
+  def index
+  end
+
+  def add
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def show
+  end
+
+end
